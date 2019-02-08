77bd2ce6f558f99b73451df2c781b91360d7ed36
Danny van Kooten <dannyvankooten@gmail.com>
2019-02-08 15:30:02 +0100

    clean-up test

--- tests/VatBundleTest.php
+++ tests/VatBundleTest.php
@@ -2,11 +2,10 @@
 
 namespace Ibericode\VatBundle\Tests\DependencyInjection;
 
-use Ibericode\Vat\Rates;
 use Ibericode\VatBundle\DependencyInjection\VatExtension;
 use Ibericode\VatBundle\VatBundle;
+
 use PHPUnit\Framework\TestCase;
-use Symfony\Component\Cache\Tests\Simple\Psr6CacheWithoutAdapterTest;
 
 class VatBundleTest extends TestCase
 {
